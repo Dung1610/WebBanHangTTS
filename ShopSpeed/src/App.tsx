d3a640534f3bd12f08392a8ad77a016eab88f4ef
Dung1610 <nguyentand1610@gmail.com>
2024-07-13 21:06:08 +0700

    update sidebar

--- ShopSpeed/src/App.tsx
+++ ShopSpeed/src/App.tsx
@@ -1,7 +1,51 @@
-export default function App() {
+import { useState } from "react";
+import { ManagerSalesman } from "./Content/ManagerSalesman";
+
+const App = () => {
+  const [open, setOpen] = useState(true);
+
   return (
-    <h1 className="text-6xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div className="flex">
+      <div
+        className={` ${
+          open ? "w-72" : "w-20 "
+        } bg-dark-purple h-screen p-5  pt-8 relative duration-300`}
+      >
+        <img
+          src="./src/image/control.png"
+          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+           border-2 rounded-full  ${!open && "rotate-180"}`}
+          onClick={() => setOpen(!open)}
+        />
+        <div className="flex gap-x-4 items-center">
+          <img
+            src="./src/image/logo.png"
+            className={`cursor-pointer duration-500 ${
+              open && "rotate-[360deg]"
+            }`}
+          />
+          <h1
+            className={`text-white origin-left font-medium text-xl duration-200 ${
+              !open && "scale-0"
+            }`}
+          >
+            Designer
+          </h1>
+        </div>
+        <ul className="pt-6">
+            <li
+              className='flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4'>
+              <img src={`./src/image/User.png`} />
+              <button className={`${!open && "hidden"} origin-left duration-200`}>
+              Accounts
+              </button>
+            </li>
+        </ul>
+      </div>
+      <div className="h-screen flex-1 p-7">
+        <ManagerSalesman/>
+      </div>
+    </div>
+  );
+};
+export default App;
---
+++ ShopSpeed/src/Content/ManagerSalesman.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ManagerSalesman = () => {
+  return (
+    <div>ManagerSalesman</div>
+  )
+}
